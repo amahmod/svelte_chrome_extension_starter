6c095c743c6bebe464279bffbb0917ed9f75d808
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2025-05-31 12:18:41 +0600

    fix: eslint linting does not work

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,8 @@ import plugin_js from '@eslint/js'
 import globals from 'globals'
 import typescript_eslint from 'typescript-eslint'
 import eslint_plugin_svelte from 'eslint-plugin-svelte'
-
-import eslint_plugin_prettier from 'eslint-config-prettier'
+import eslint_plugin_prettier from 'eslint-plugin-prettier'
+import eslint_config_prettier from 'eslint-config-prettier'
 
 export default [
     // js
@@ -12,9 +12,37 @@ export default [
     // ts
     ...typescript_eslint.configs.recommended,
     {
+        files: ['**/*.ts', '**/*.tsx', '**/*.svelte'],
         languageOptions: {
-            globals: globals.browser,
-            sourceType: 'module'
+            globals: {
+                ...globals.browser,
+                ...globals.es2021
+            },
+            sourceType: 'module',
+            parser: typescript_eslint.parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module'
+            }
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_' }
+            ],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            // Disable base ESLint rules that are handled by TypeScript ESLint
+            'no-unused-expressions': 'off',
+            '@typescript-eslint/no-unused-expressions': [
+                'error',
+                {
+                    allowShortCircuit: true,
+                    allowTernary: true,
+                    allowTaggedTemplates: true
+                }
+            ]
         }
     },
 
@@ -27,10 +55,24 @@ export default [
                 extraFileExtensions: ['.svelte'],
                 parser: typescript_eslint.parser
             }
+        },
+        rules: {
+            'svelte/valid-compile': 'error',
+            'svelte/no-at-html-tags': 'error',
+            'svelte/no-dom-manipulating': 'error'
         }
     },
 
     // prettier
-    ...eslint_plugin_svelte.configs['flat/prettier'],
-    eslint_plugin_prettier
+    eslint_config_prettier,
+    {
+        plugins: {
+            prettier: eslint_plugin_prettier
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'arrow-body-style': 'off',
+            'prefer-arrow-callback': 'off'
+        }
+    }
 ]
